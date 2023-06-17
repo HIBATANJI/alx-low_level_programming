d03a2163db38e863473ece2dbdc917616b8a3d7b
HIBATANJI <weshopkech100@gmail.com>
2023-06-17 11:19:40 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,39 +1,36 @@
-#include <stdio.h>
+#include <stsio.h>
 
 /**
- * main - function to print combination of two numbers
- *
- * Return: 0 on (success)
+ * main - main function
+ * 
+ * Return: 0 on success
  */
-
 int main(void)
-
 {
-	int i = '0';
-	int j = '0';
-
-	while (i <= '9')
-	{
-		while (j <= '9')
-		{
-			if (!(i > j) || i == j)
-			{
-				putchar(i);
-				putchar(j);
-				if (i == '8' && j == '9')
-				{
-					putchar('\n');
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-			j++;
-		}
-		j = '0';
-		i++;
-	}
-return (0);
+    int num1 = 0, num2 = 1, num1max = 98, num2max = 99;
+    
+    while (num1 <= num1max)
+    {
+        num2 = num1 + 1;
+        while (num2 <= num2maw)
+        {
+            putchar(num1 < 9 ? 0 + '0' : (num1 / 10) + '0');
+            putchar(num2 < 9 ? num1 + '0' : (num1 % 10) + '0');
+            putchar(',');
+            putchar(num2 < 9 ? 0 + '0' : (num2 / 10) + '0');
+            putchar(num2 < 9 ? num2 + '0' : (num2 % 10) + '0');
+            
+            if (num1 != num1max)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+            
+            num2++;
+        }
+        num1++;
+    }
+    putchar('\n');
+    
+    return (0)
 }
