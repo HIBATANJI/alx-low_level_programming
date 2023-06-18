c0a27e1a26c8cf714840ffde7ce9e9c007d1d3dc
HIBATANJI <weshopkech100@gmail.com>
2023-06-18 14:56:09 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
-* main - print all possible differant combination of two digits.
-* 
-* Return: Always 0.
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-    int x, y;
-     
-    for (x = 0; y < 9; x++)
-    {
-        for (y = x + 1; y < 10; y++)
-        {
-            putchar((x % 10) + '0');
-            putchar((y % 10) + '0');
-            
-            if (x == 8 && y == 9)
-               continue;
-               
-           putchar(',');
-           putchar(' ');
-           
-           
-        }
-    }
-    
-    putchar('\n');
-    
-    return (0);
+	int d, p;
+
+	for (d = '0'; d < '9'; d++)
+	{
+		for (p = d + 1; p <= '9'; p++)
+		{
+			if (p != d)
+			{
+				putchar(d);
+				putchar(p);
+
+				if (d == '8' && p == '9')
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+
 }
