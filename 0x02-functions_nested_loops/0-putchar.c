812716cde7f1fb70b1b334020a62c5d5623d0eee
HIBATANJI <weshopkech100@gmail.com>
2023-06-20 06:31:37 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,9 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * main - prints Putchar
- *
- * Description: prints _putchar
- *
- * Return: 0 (Success)
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	_putchar('_');
