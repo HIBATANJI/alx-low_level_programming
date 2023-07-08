eaa757217c0af4a8ccc68646eaa446d5928ae54c
HIBATANJI <weshopkech100@gmail.com>
2023-07-07 18:04:12 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,40 +1,118 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
 
 /**
- * main - Entry point of the program
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
- *
- * Return: Always 0 (Success)
+ * check_str_number - checks if string contains numbers
+ * @c: the string
+ * Return: returns 1 if given string only contains numbers, 0 otherwise.
+ **/
+
+int check_str_number(char *c)
+{
+	while (*c)
+	{
+		if (*c >= '0' && *c <= '9')
+			c++;
+		else
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * str_len - calculate the length of a string
+ * @s: the string
+ * Return: return the length
  */
+
+int str_len(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * _atoi - converts a string to int
+ * @s: the string to convert
+ * Return: return the value of the string converted
+ */
+
+int _atoi(char *s)
+{
+	int result;
+	int sign;
+	int i;
+	int len;
+
+	i = 0;
+	len = str_len(s);
+	sign = 1;
+	result = 0;
+	while (s[i] < '0' || s[i] > '9')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	if (len > i)
+	{
+		if (sign == -1)
+		{
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				result *= 10;
+				result -= s[i] - 48;
+				i++;
+			}
+		}
+		else
+		{
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				result *= 10;
+				result += s[i] - 48;
+				i++;
+			}
+		}
+	}
+	return (result);
+}
+
+/**
+ * main - Entry point
+ * Description: prints the number of arguments.
+ * @argc: the number of arguments.
+ * @argv: the array of arguments.
+ * Return: always 0.
+ */
+
 int main(int argc, char *argv[])
 {
 	int i;
-	int sum = 0;
+	int sum;
 
-	if (argc == 1)
+	sum = 0;
+	i = 1;
+	while (i < argc)
 	{
-		printf("0\n");
-		return (0);
-	}
-
-	for (i = 1; i < argc; i++)
-	{
-		int j;
-		for (j = 0; argv[i][j] != '\0'; j++)
+		if (check_str_number(argv[i]))
 		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
+			printf("Error\n");
+			return (1);
 		}
-		sum += atoi(argv[i]);
+		i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		sum += _atoi(argv[i]);
+		i++;
 	}
 
 	printf("%d\n", sum);
-
 	return (0);
 }
